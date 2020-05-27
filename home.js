80d861c3aa5b2ce14ce0083c8dec6ac8df19f485
lukenfc <luke@nfcideas.com>
2020-05-27 14:56:29 -0400

    blue background

--- home.js
+++ home.js
@@ -1,3 +1,4 @@
+
 function generateSettings(){
     if(localStorage.getItem('settings') === null){
         var settings = {
@@ -7,13 +8,8 @@ function generateSettings(){
             timeLimit: 5,
             p1name: 'Player 1',
             p2name: 'Player 2',
-            difficulty: 1
+            difficulty: 1,
         }
         localStorage.setItem('settings', JSON.stringify(settings));
     }
-    const red = Math.floor(Math.random()*256);
-    const green = Math.floor(Math.random()*256);
-    const blue = Math.floor(Math.random()*256);
-    document.body.style.backgroundColor = 'rgb('+red+','+green+','+blue+')'
-    console.log(getComputedStyle(document.body).backgroundColor)
 }
