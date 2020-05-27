e5925843d987498c51a3063ddc7993d30de463f6
lukenfc <luke@nfcideas.com>
2020-05-27 13:05:52 -0400

    random background

--- home.js
+++ home.js
@@ -11,4 +11,9 @@ function generateSettings(){
         }
         localStorage.setItem('settings', JSON.stringify(settings));
     }
+    const red = Math.floor(Math.random()*256);
+    const green = Math.floor(Math.random()*256);
+    const blue = Math.floor(Math.random()*256);
+    document.body.style.backgroundColor = 'rgb('+red+','+green+','+blue+')'
+    console.log(getComputedStyle(document.body).backgroundColor)
 }
